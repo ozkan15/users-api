616a9e1a88556e42df3ac76b486f41ff2ccf12d9
ozkan15 <akifozkan_@hotmail.com>
2018-11-05 18:14:41 +0300

    user modeli oluşturuldu

---
+++ api/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('../../node_modules/mongoose');
+
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
